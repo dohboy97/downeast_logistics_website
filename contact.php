8330145ac26957af7dff072a3c9e0c143898a466
Connor <connor.doherty97@gmail.com>
2022-07-31 18:54:46 -0400

    phpmailer framework now successfully sending emails

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 require 'vendor/autoload.php';
 
-if (array_key_exists('to', $_POST)) {
+if (array_key_exists('subject', $_POST)) {
     $err = false;
     $msg = '';
     $email = '';
@@ -36,11 +36,11 @@ if (array_key_exists('to', $_POST)) {
     //Never allow arbitrary input for the 'to' address as it will turn your form into a spam gateway!
     //Substitute appropriate addresses from your own domain, or simply use a single, fixed address
     if (array_key_exists('to', $_POST) && in_array($_POST['to'], ['sales', 'support', 'accounts'], true)) {
-        $to = $_POST['to'] . 'connor.doherty97@gmail.com';
+        $to = 'connor.doherty97@gmail.com';
     } else {
         $to = 'connor.doherty97@gmail.com';
     }
-    //Make sure the address they provided is valid before trying to use it
+    //Make sure the addinfo@downeastlog.com before trying to use it
     if (array_key_exists('email', $_POST) && PHPMailer::validateAddress($_POST['email'])) {
         $email = $_POST['email'];
     } else {
@@ -48,16 +48,23 @@ if (array_key_exists('to', $_POST)) {
         $err = true;
     }
     if (!$err) {
+
         $mail = new PHPMailer();
+
+        $mail->SMTPDebug = 2;
+
         $mail->isSMTP();
-        $mail->Host = 'localhost';
+        $mail->Host = 'smtp.ipower.com';
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'sales@downeastlogistics.com';                 // SMTP username
+        $mail->Password = 'Bmca&h!S9k8#517';
         $mail->Port = 25;
         $mail->CharSet = PHPMailer::CHARSET_UTF8;
         //It's important not to use the submitter's address as the from address as it's forgery,
         //which will cause your messages to fail SPF checks.
         //Use an address in your own domain as the from address, put the submitter's address in a reply-to
-        $mail->setFrom('info@downeastlog.com', (empty($name) ? 'Contact form' : $name));
-        $mail->addAddress($to);
+        $mail->setFrom('sales@downeastlogistics.com', (empty($name) ? 'Contact form' : $name));
+        $mail->addAddress('connor.doherty97@gmail.com');
         $mail->addReplyTo($email, $name);
         $mail->Subject = 'Contact form: ' . $subject;
         $mail->Body = "Contact form submission\n\n" . $query;
@@ -67,45 +74,7 @@ if (array_key_exists('to', $_POST)) {
             $msg .= 'Message sent!';
         }
     }
-}
-
-// $message_sent = false;
- //submit form
-// if(isset($_POST['email']) && $_POST['email']!= ''){
-
-//     if(filter_var($_POST['visitor_email'], FILTER_VALIDATE_EMAIL)){
-//         $userName = $_POST['visitor_name'];
-//         $userEmail = $_POST['visitor_email'];
-//         $userSubject = $_POST['visitor_subject'];
-//         $userMessage = $_POST['visitor_message'];
-        
-       
-       
-//         $TO = 'connor.doherty97@gmail.com';
-//         $body = '';
-       
-//         $body .= 'From: '.$userName. '\r\n';
-//         $body .= 'Email: '.$userEmail. '\r\n';
-//         $body .= 'Message: '.$userMessage. '\r\n';
-        
-//         mail($to,$userSubject,$body);
-//         $message_sent = true;
-//         if(mail($to, $userSubject, $body)) {
-//             echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
-//         } else {
-//             echo '<p>We are sorry but the email did not go through.</p>';
-//         }
-//     }
-//     else{
-//         $message_sent = false;
-//     }
-
-
- 
-// }
-
-
-?>
+} ?>
 
 
 <html lang="en">
@@ -232,7 +201,7 @@ if (array_key_exists('to', $_POST)) {
                     
                     
                     <label for = 'query' class = 'form-label'>Message:</label>
-                    <textarea width = '100%' height = '150px' id = 'query' name='query' placeholder="Message..."></textarea>
+                    <textarea style = 'width:100%; height:150px' id = 'query' name='query' placeholder="Message..."></textarea>
                 </div>
                 <button type = 'submit'>Send Message</button>
             </form>
